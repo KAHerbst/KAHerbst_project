37b608e9655519d237a5ae1e4c528454bc88f23d
KAHerbst <kah3@williams.edu>
2016-12-02 10:21:36 -0500

    Update poverty.py

--- poverty.py
+++ poverty.py
@@ -10,7 +10,7 @@ def mercator(lat):
     projection = math.log(math.tan((math.pi / 4) + (lat_rad / 2)))
     return (180 * projection) / math.pi
 
-def main(results, boundaries, output, width, style):
+def main(results, boundaries, output, width):
     """
     Draws an image.
     This function creates an image object, constructs Region objects by reading
@@ -21,7 +21,6 @@ def main(results, boundaries, output, width, style):
         boundaries (str): name of a csv file of geographic information
         output (str): name of a file to save the image
         width (int): width of the image
-        style (str): either 'GRAD' or 'SOLID'
     """
     def to_point(coords):
         new_coords=[]
@@ -31,7 +30,7 @@ def main(results, boundaries, output, width, style):
         return new_coords
 
     with open(boundaries, 'r') as f1, open (results, 'r') as f2:
-        region_list = [Region(to_point(bounds),float(voter_percent[2]),float(voter_percent[3]),float(voter_percent[4])) for bounds,voter_percent in zip(csv.reader(f1),csv.reader(f2))]
+        region_list = [Region(to_point(bounds),float(poverty_percent[10])) for bounds,poverty_percent in zip(csv.reader(f1),csv.reader(f2))]
     regionminlat = min([region.min_lat() for region in region_list])
     regionmaxlat = max([region.max_lat() for region in region_list])
     regionminlong = min([region.min_long() for region in region_list])
@@ -39,16 +38,15 @@ def main(results, boundaries, output, width, style):
 
     region_plot = Plot(width,regionminlong,regionminlat,regionmaxlong,regionmaxlat)
     for region in region_list:
-        region_plot.draw(region,style)
+        region_plot.draw(region)
     region_plot.save(output)
 
 
-
+#I don't care about the style it will always be gradient
 
 if __name__ == '__main__':
-    results = sys.argv[1]
+    poverty_data = sys.argv[1]
     boundaries = sys.argv[2]
     output = sys.argv[3]
     width = int(sys.argv[4])
-    style = sys.argv[5]
     main(results, boundaries, output, width, style)
