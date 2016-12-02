3d3b4dcfaae1de8ce078f3ba7b750703b1bb7ccc
KAHerbst <kah3@williams.edu>
2016-12-02 11:34:19 -0500

    Update file_trimmer.py

--- file_trimmer.py
+++ file_trimmer.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import sys
+#Does the intial trimming of removing the rows I do not want to use
 f=pd.read_csv(data)
 keep_col = ['State / County Name' , 'All Ages in Poverty Percent']
 new_f = f[keep_col]
 new_f.to_csv(trimmed, index=False)
-
+#Now I have to figure out reformat the county and state data
 
 if __name__ == '__main__':
     data = sys.argv[1]
