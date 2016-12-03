b31a6613eccf610878268a6770631d22709cecab
KAHerbst <kah3@williams.edu>
2016-12-03 15:45:21 -0500

    Update file_formatter.py

--- file_formatter.py
+++ file_formatter.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sys
 import csv
 
+
 f=pd.read_csv('poverty_data.csv')
 keep_col = ['State / County Name' , 'All Ages in Poverty Percent']
 new_f = f[keep_col]
@@ -14,17 +15,24 @@ with open('poverty_trimmed.csv', 'w') as fout:
             if rownum<1:continue
             writer.writerow(entry)
 
+with open('boundaries.csv' ,'r') as bounds_initial:
+    with open('boundaries_trimmed.csv', 'w') as bounds:
+        next(bounds_initial)
+        for row in bounds_initial:
+            bounds.write(row)
+
 poverty_dict = {}
 with open('poverty_trimmed.csv','r') as trim:
     trim_read = csv.reader(trim)
     for row in trim_read:
         lst = row[0].split()
         poverty_dict[lst[0]] = row
-with open('boundaries.csv', 'r') as bounds:
+with open('boundaries_trimmed.csv', 'r') as bounds:
     boundaries = csv.reader(bounds)
     with open('poverty_formatted.csv', 'w') as pov_final:
         writer = csv.writer(pov_final)
         for row in boundaries:
-            if row[0] not in poverty_dict:
-                poverty_dict[row[0]] = [row[0], '14.5']
-            writer.writerow(poverty_dict[row[0]])
+            row_lst = row[3].split()
+            if row_lst[0] not in poverty_dict:
+                poverty_dict[row_lst[0]] = [row_lst[0], '14.5']
+            writer.writerow(poverty_dict[row_lst[0]])
