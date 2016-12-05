6f24a68945c04c955bccc40b0fc639500ad6bb1c
KAHerbst <kah3@williams.edu>
2016-12-05 11:33:59 -0500

    Update and rename poverty.py to images.py
    Added function to generate SNAP and Pov graphs

--- images.py
+++ images.py
@@ -3,6 +3,8 @@ import csv
 import math
 from region import Region
 from plot import Plot
+import numpy as np
+import matplotlib.pyplot as plt
 
 def mercator(lat):
     """project latitude 'lat' according to Mercator"""
@@ -36,14 +38,33 @@ def main(poverty, boundaries, output, width, style):
     regionmaxlat = max([region.max_lat() for region in region_list])
     regionminlong = min([region.min_long() for region in region_list])
     regionmaxlong = max([region.max_long() for region in region_list])
-    
+
 
     region_plot = Plot(width,regionminlong,regionminlat,regionmaxlong,regionmaxlat)
     for region in region_list:
         region_plot.draw(region,style)
     region_plot.save(output)
 
+def subplots(filename,year_cap):
+    pov_lst = [11.3, 11.7, 12.1, 12.5, 12.7, 13.3, 13.3, 13.0, 13.2, 14.3, 15.3, 15.9, 15.9, 15.8, 15.5]
 
+    SNAP_lst = [17.194000, 17.318000, 19.096000, 21.250000, 23.811000, 25.628000, 26.549000, 26.316000, 28.223000, 33.490000, 40.302000, 44.709000, 46.609000, 47.636000, 46.664000]
+    years = [x for x in range(year_cap-2000)]
+
+    plt1 = plt.subplot2grid((4,10),(0,0), colspan = 9, rowspan = 2)
+    for x in years:
+        plt1.scatter(x,pov_lst[x])
+
+    plt2 = plt.subplot2grid((4,10),(2,0), colspan = 9, rowspan = 2)
+    for x in years:
+        plt2.scatter(x,SNAP_lst[x])
+
+
+    plt2.set_xlabel('Year')
+    plt1.set_ylabel('Poverty Percentage')
+    plt2.set_ylabel('SNAP Enrollment (In Millions)')
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -52,4 +73,6 @@ if __name__ == '__main__':
     output = sys.argv[3]
     width = int(sys.argv[4])
     style = sys.argv[5]
+    year_cap = sys.argv[6]
     main(poverty, boundaries, output, width, style)
+    subplots(plots,year_cap)
